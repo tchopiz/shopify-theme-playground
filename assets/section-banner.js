f342e81b64f83794c622a01be39b73df9827d771
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-08-24 16:06:01 +0300

    fix button ripple effect

--- assets/section-banner.js
+++ assets/section-banner.js
@@ -8,14 +8,20 @@ const getCookies = (name) => {
 
 function createRipple(event) {
   const button = event.currentTarget;
-
+  console.log("button", button);
+  console.log("event.clientY", event.clientY);
+  console.log("event.clientX", event.clientX);
+  console.log(button.offsetTop);
   const circle = document.createElement("span");
   const diameter = Math.max(button.clientWidth, button.clientHeight);
   const radius = diameter / 2;
-
   circle.style.width = circle.style.height = `${diameter}px`;
-  circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
-  circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+  circle.style.left = `${
+    event.clientX - button.getBoundingClientRect().left - radius
+  }px`;
+  circle.style.top = `${
+    event.clientY - button.getBoundingClientRect().top - radius
+  }px`;
   circle.classList.add("ripple");
 
   const ripple = button.getElementsByClassName("ripple")[0];
@@ -29,3 +35,5 @@ function createRipple(event) {
 
 const button = document.getElementById("shop-upet-button");
 button.addEventListener("click", createRipple);
+
+// mdc.ripple.MDCRipple.attachTo(document.querySelector(".foo-button"));
